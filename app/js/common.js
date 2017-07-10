b2da966bf5940cdb79e9da71a70c9406e2bdfa9f
Yevgheny <yevgheny78@gmail.com>
2017-07-10 16:38:03 +0600

    слайдер на главном экране

--- app/js/common.js
+++ app/js/common.js
@@ -24,6 +24,18 @@ $(function() {
 
 });
 $(document).ready(function() {
+    //nav-menu
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+
+    $('.overlay').on('click', function(){
+        $(".overlay").fadeToggle(200);   
+        $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+        open = false;
+    });
+    
     $('#slider').owlCarousel({
         loop: true,
         margin: 10,
